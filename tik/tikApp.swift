e04bd96dd4099c5834b3cc23f0a1ab24dd774c35
lizahjortling <lizhj90@outlook.com>
2023-05-12 08:58:24 +0200

    init

---
+++ tik/tikApp.swift
@@ -0,0 +1,17 @@
+//
+//  tikApp.swift
+//  tik
+//
+//  Created by Liza Hjortling on 2023-05-12.
+//
+
+import SwiftUI
+
+@main
+struct tikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tikUITests/tikUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  tikUITestsLaunchTests.swift
+//  tikUITests
+//
+//  Created by Liza Hjortling on 2023-05-12.
+//
+
+import XCTest
+
+final class tikUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
