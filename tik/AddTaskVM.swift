9e22e261940f71c0b7ec38cb381433657462fd77
tobbesoren <113234609+tobbesoren@users.noreply.github.com>
2023-05-20 14:35:06 +0200

    addTaskViewIsAdded

---
+++ tik/AddTaskVM.swift
@@ -0,0 +1,8 @@
+//
+//  AddTaskVM.swift
+//  tik
+//
+//  Created by Tobias Sörensson on 2023-05-20.
+//
+
+import Foundation
---
+++ tik/Subviews/AddTaskView.swift
@@ -0,0 +1,20 @@
+//
+//  AddTaskView.swift
+//  tik
+//
+//  Created by Tobias Sörensson on 2023-05-20.
+//
+
+import SwiftUI
+
+struct AddTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddTaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddTaskView()
+    }
+}
