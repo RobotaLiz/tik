99f063822517c9799f2afb5702120f532c0784d3
tobbesoren <113234609+tobbesoren@users.noreply.github.com>
2023-05-31 11:04:44 +0200

    hmm

--- tik/Models/Member.swift
+++ tik/Models/Member.swift
@@ -2,7 +2,12 @@
 //  Member.swift
 //  tik
 //
-//  Created by Tobias Sörensson on 2023-05-31.
+//  Created by Tobias Sörensson on 2023-05-26.
 //
 
 import Foundation
+
+struct Member : Codable, Equatable {
+    var userID : String
+    var admin : Bool
+}
