61ab3538ee87253d237cf8fadf402831782ebc7f
tobbesoren <113234609+tobbesoren@users.noreply.github.com>
2023-05-31 12:03:38 +0200

    stuffToDo

--- tik/AddTaskVM.swift
+++ tik/AddTaskVM.swift
@@ -1,28 +1,28 @@
+////
+////  AddTaskVM.swift
+////  tik
+////
+////  Created by Tobias Sörensson on 2023-05-20.
+////
 //
-//  AddTaskVM.swift
-//  tik
+//import Foundation
+//import Firebase
+//import FirebaseAuth
 //
-//  Created by Tobias Sörensson on 2023-05-20.
-//
-
-import Foundation
-import Firebase
-import FirebaseAuth
-
-class AddTaskVM: ObservableObject {
-    let auth = Auth.auth()
-    let db = Firestore.firestore()
-    
-    func saveToFirestore(task: Task) {
-        // This is preliminary. Saves to the user - should save to the Household.
-        guard let user = auth.currentUser else {return}
-        
-        let taskRef = db.collection("users").document(user.uid).collection("tasks")
-        
-        do {
-            try taskRef.addDocument(from: task)
-        } catch {
-            print("Error saving to db.\(error)")
-        }
-    }
-}
+//class AddTaskVM: ObservableObject {
+//    let auth = Auth.auth()
+//    let db = Firestore.firestore()
+//    
+//    func saveToFirestore(task: Task) {
+//        // This is preliminary. Saves to the user - should save to the Household.
+//        guard let user = auth.currentUser else {return}
+//        
+//        let taskRef = db.collection("users").document(user.uid).collection("tasks")
+//        
+//        do {
+//            try taskRef.addDocument(from: task)
+//        } catch {
+//            print("Error saving to db.\(error)")
+//        }
+//    }
+//}
