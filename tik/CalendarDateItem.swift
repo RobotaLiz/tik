2b37c5630a437a7f4450048fc9552fa2210c279b
Tibalomium <ninjaharry@gmail.com>
2023-06-02 01:10:18 +0200

    cleaned up a tiny bit

---
+++ tik/CalendarDateItem.swift
@@ -0,0 +1,113 @@
+//
+//  CalendarDateItem.swift
+//  tik
+//
+//  Created by Hakan Johansson on 2023-05-30.
+//
+
+import Foundation
+import SwiftUI
+
+enum ItemState {
+    case selected, available, notAvailable, lastMonth, nextMonth
+}
+
+struct CalendarDateItem: View {
+    let date: Date
+    let calendarVM: CalendarViewModel
+    @State var itemState : ItemState = .notAvailable
+    
+    var body: some View {
+        let calendar = Calendar.current
+        let day = calendar.dateComponents([.day], from: date).day ?? 0
+        
+        ZStack {
+            if day < 10 {
+                circle
+                Text("\(day)")
+            }
+            else {
+                circle
+                Text("\(day)")
+            }
+        }
+        .onTapGesture {
+            pressed()
+        }
+        .onAppear {
+            setState()
+        }
+        .onReceive(calendarVM.$currentMonth) { month in
+            setState()
+        }
+    }
+    
+    func setState() {
+        let calendar = Calendar.current
+        
+        if let nextMonth = calendar.date(byAdding: DateComponents(month: 1), to: calendarVM.currentMonth),
+           let lastMonth = calendar.date(byAdding: DateComponents(month: -1), to: calendarVM.currentMonth) {
+            if calendar.isDate(date, equalTo: nextMonth, toGranularity: .month) {
+                itemState = .nextMonth
+            }
+            else if calendar.isDate(date, equalTo: lastMonth, toGranularity: .month) {
+                itemState = .lastMonth
+            }
+            else if calendarVM.dateIsSelected(date: date) {
+                itemState = .selected
+            }
+            else if calendarVM.dateIsInTaskList(date: date) {
+                itemState = .available
+            }
+            else {
+                itemState = .notAvailable
+            }
+        }
+    }
+    
+    func pressed() {
+        let calendar = Calendar.current
+        
+        if let nextMonth = calendar.date(byAdding: DateComponents(month: 1), to: calendarVM.currentMonth),
+           let lastMonth = calendar.date(byAdding: DateComponents(month: -1), to: calendarVM.currentMonth) {
+            print(calendar.isDate(date, equalTo: nextMonth, toGranularity: .month))
+            print(date)
+            print(calendarVM.currentMonth)
+            if calendar.isDate(date, equalTo: nextMonth, toGranularity: .month) {
+                itemState = .nextMonth
+                calendarVM.currentMonth = nextMonth
+            }
+            else if calendar.isDate(date, equalTo: lastMonth, toGranularity: .month) {
+                itemState = .lastMonth
+                calendarVM.currentMonth = lastMonth
+            }
+            else if calendarVM.dateIsSelected(date: date) {
+                itemState = .selected
+                calendarVM.toggleTask(date: date)
+            }
+            else if calendarVM.dateIsInTaskList(date: date) {
+                itemState = .available
+                calendarVM.toggleTask(date: date)
+
+            }
+            else {
+                itemState = .notAvailable
+            }
+        }
+    }
+    
+    var circle: some View {
+        switch itemState {
+        case .available:
+            return Circle().fill(.yellow)
+        case .notAvailable:
+            return Circle().fill(.pink)
+        case .selected:
+            return Circle().fill(.blue)
+        case .lastMonth:
+            return Circle().fill(.gray)
+        case .nextMonth:
+            return Circle().fill(.gray)
+        }
+    }
+}
