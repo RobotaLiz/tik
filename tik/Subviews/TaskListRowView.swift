3bdde5cc3bbeb9e6cc9d58f1d3cf1cb53a2bb536
Antonio Saracino <104855075+braindome@users.noreply.github.com>
2023-06-03 22:01:31 +0200

    Added second row to TaskListRowView for assigining members to task

--- tik/Subviews/TaskListRowView.swift
+++ tik/Subviews/TaskListRowView.swift
@@ -12,23 +12,76 @@ struct TaskListRowView: View {
     var task : Task
     //var isDone : Bool
     @EnvironmentObject var firestoreManagerViewModel : FirestoreManagerVM
+    @State var mockMembers = ["Antonio", "Tobias", "Liza", "Miguel", "Håkan"]
+    @State var selectedMembers : [String] = []
     
     var body: some View {
-        HStack {
-            Text(task.title)
-            Spacer()
-            Button(action: {
-                firestoreManagerViewModel.toggleTikBox(task: task)
-            }) {
-                if task.isCompleted {
-                    Image(systemName: "checkmark.square")
-                } else {
-                    Image(systemName: "square")
+        VStack {
+            HStack {
+                Text(task.title)
+                Spacer()
+                Button(action: {
+                    firestoreManagerViewModel.toggleTikBox(task: task)
+                }) {
+                    if task.isCompleted {
+                        Image(systemName: "checkmark.square")
+                    } else {
+                        Image(systemName: "square")
+                    }
                 }
+                .buttonStyle(PlainButtonStyle())
             }
-            .buttonStyle(PlainButtonStyle())
+            Divider()
+            HStack {
+                Menu {
+                    ForEach(mockMembers, id: \.self) { member in
+                        Button(member) {
+                            assignSelectedMemberToTask(memberName: member)
+                        }
+                    }
+                } label: {
+                    Label("", systemImage: "person.fill.badge.plus")
+                        .font(.system(size: 26))
+                }
+                
+                ForEach(selectedMembers, id: \.self) { selectedMember in
+                    Text(selectedMember)
+                }
+                
+                Button(action: {
+                    assignMyselfToTask()
+                }) {
+                    Image(systemName: "figure.wave.circle.fill")
+                        .font(.system(size: 26))
+                    
+                }
+                
+            }
+        }
+        .cornerRadius(10)
+    }
+    
+    
+    // TODO: Integration with firestore, integration in view model, removing mock data, add task listener, styling??
+    func assignMyselfToTask() {
+        if selectedMembers.contains("Current User") {
+            selectedMembers.removeAll { $0 == "Current User" }
+        } else {
+            selectedMembers.append("Current User")
         }
     }
+    
+    func assignSelectedMemberToTask(memberName: String) {
+        if selectedMembers.contains(memberName) {
+            selectedMembers.removeAll { $0 == memberName }
+        } else {
+            selectedMembers.append(memberName)
+        }
+    }
+
+    
+    
+
 }
 
 struct TaskListRowView_Previews: PreviewProvider {
--- tik/TaskListView.swift
+++ tik/TaskListView.swift
@@ -11,26 +11,11 @@ import FirebaseAuth
 struct TaskListView: View {
     // Tobbe's dirty fingers are everywhere...
     @State var addTaskIsPresented = false
-    //@ObservedObject var authViewModel : FirestoreManagerVM
     @EnvironmentObject var firestoreManagerViewModel : FirestoreManagerVM
-    // Antonio's side bar stuff - Work in progress
-    @State var isSidebarOpen = false
-    @State var selectedTag: String? = nil
-    @State var selectedTab : Int? = 0
     
     let auth = Auth.auth()
     
     @Environment(\.presentationMode) var presentationMode
-    
-    var selectedTagBinding: Binding<String?> {
-        Binding<String?>(
-            get: { selectedTag },
-            set: { tag in
-                selectedTag = tag
-                isSidebarOpen = tag != nil
-            }
-        )
-    }
     
     @State var mockData = [Task(title: "Clean kitchen floor", setDate: Date()),
                            Task(title: "Dust living room", setDate: Date()),
@@ -99,11 +84,6 @@ struct TaskListView: View {
             .navigationViewStyle(.stack)
             .navigationBarTitle("Tasks")
             .listStyle(.plain)
-            .onAppear {
-                if !isSidebarOpen {
-                    isSidebarOpen = false
-                }
-            }
             .navigationBarItems(trailing: {
                 Menu {
                     Button(action: {
