426786d433ad51f80513c7f3b02ae48f12867a47
MiguelMalungo <98886465+MiguelMalungo@users.noreply.github.com>
2023-06-07 13:40:16 +0100

    changes

--- tik/Subviews/CreateHouseholdView.swift
+++ tik/Subviews/CreateHouseholdView.swift
@@ -32,12 +32,12 @@ struct CreateHouseholdView: View {
                 }
 //                Spacer().frame(height: 180)
                 HStack {
-                    Spacer(minLength: 50)
+                    Spacer(minLength: 60)
                     TextField("Enter name", text: $householdName)
                         .padding(10)
                         .cornerRadius(10)
                         .border(Color(.black))
-                    Spacer(minLength: 50)
+                    Spacer(minLength: 60)
                 }
                 .padding(20)
                 Button(action: {
@@ -67,12 +67,14 @@ struct CreateHouseholdView: View {
                         Text("Add Household")
                             .font(.custom("Roboto-Bold", size: 24))
                             .foregroundColor(.black)
+                            .padding(.bottom)
                         
                         Image(systemName: "plus.circle.fill")
                             .resizable()
                             .frame(width: 50, height: 50)
                             .foregroundColor(.black)
                     }
+                    .padding(.bottom, 70.0)
                 }
                 
             }
