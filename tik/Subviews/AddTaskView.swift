bea621453a563bb023801214d24afd0ce73473e0
tobbesoren <113234609+tobbesoren@users.noreply.github.com>
2023-06-08 19:18:25 +0200

    aStart

--- tik/Subviews/AddTaskView.swift
+++ tik/Subviews/AddTaskView.swift
@@ -23,126 +23,88 @@ struct AddTaskView: View {
     @EnvironmentObject var firestoreManagerViewModel : FirestoreManagerVM
     
     var body: some View {
-        NavigationView {
-            ZStack {
-                // Add your image here
-                Image("Two Phone Mockup Download App Instagram Post(10)")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .edgesIgnoringSafeArea(.all)
+//        NavigationView {
+            ZStack (alignment: .center) {
                 
-                // Big yellow circle
-                Circle()
-                    .fill(Color.yellow)
-                    .frame(width: 300, height: 300)
-                    .offset(y: -180)
-                VStack(alignment: .center) {
-                    Spacer().frame(height: 90)
-                    TextField("What to do:", text: $title)
-                        .font(.custom("Roboto-Bold", size: 22))
-                        .multilineTextAlignment(.center)
-                        .padding()
-                    TextField("How to do it:", text: $notes)
-                        .font(.custom("Roboto-Bold", size: 22))
-                        .multilineTextAlignment(.center)
-                        .padding()
-                    TextField("Where to do it:", text: $location)
-                        .font(.custom("Roboto-Bold", size: 22))
-                        .multilineTextAlignment(.center)
-                        .padding()
+                
+                    Image("Two Phone Mockup Download App Instagram Post(10)")
+                        .resizable()
+                        .scaledToFill()
+                        .edgesIgnoringSafeArea(.all)
                     
+                    // Your other views here...
                     
-                    //                    Text("When to do it:")
-                    VStack {
-                        Spacer().frame(height: 70)
-                        Text("When to do it:")
-                            .font(.title)
-                            .bold()
-                        .foregroundColor(.black)
-                        DatePicker("", selection: $setDate)
-                            .datePickerStyle(WheelDatePickerStyle())
-                            .labelsHidden()
-                        HStack {
-                            Toggle("Alert", isOn: $addNotification).fixedSize()
-                                .tint(Color.appYellow)
-                            DatePicker("Time for notification", selection: $notificationDate, in: Date.now...)
+                    // Big yellow circle
+                    Circle()
+                    
+                        .fill(Color.yellow)
+                        .frame(width: 300, height: 300)
+                        .offset(y: -180)
+                    VStack(alignment: .center) {
+                        Spacer().frame(height: 90)
+                        TextField("What to do:", text: $title)
+                            .font(.custom("Roboto-Bold", size: 22))
+                            .multilineTextAlignment(.center)
+                            .padding()
+                        TextField("How to do it:", text: $notes)
+                            .font(.custom("Roboto-Bold", size: 22))
+                            .multilineTextAlignment(.center)
+                            .padding()
+                        TextField("Where to do it:", text: $location)
+                            .font(.custom("Roboto-Bold", size: 22))
+                            .multilineTextAlignment(.center)
+                            .padding()
+                        
+                        
+                        //                    Text("When to do it:")
+                        VStack {
+                            Spacer().frame(height: 70)
+                            Text("When to do it:")
+                                .font(.title)
+                                .bold()
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(.black)
+                            DatePicker("", selection: $setDate)
+                                .datePickerStyle(WheelDatePickerStyle())
                                 .labelsHidden()
+                            HStack {
+                                Toggle("Alert", isOn: $addNotification).fixedSize()
+                                    .tint(Color.appYellow)
+                                DatePicker("Time for notification", selection: $notificationDate, in: Date.now...)
+                                    .labelsHidden()
+                            }
+                            
                         }
+                        Spacer().frame(height: 70)
+                        Button("Add", action: {
+                            
+                            if let currentTikUser = firestoreManagerViewModel.currentTikUser {
+                                
+                                // save to firestore.
+                                let newTask = Task(title: title,
+                                                   notes: notes,
+                                                   location: location,
+                                                   assignedTo: [],
+                                                   setDate: setDate)
+                                
+                                firestoreManagerViewModel.saveTaskToFirestore(task: newTask)
+                                addTaskIsPresented = false
+                            }
+                        })
+                        .buttonStyle(CustomButtonStyle())
+                        .navigationBarItems(trailing: Button {
+                            addTaskIsPresented = false
+                        } label: {
+                            Image(systemName: "xmark")
+                                .foregroundColor(.black)
+                        })
                         
                     }
-                    //Spacer().frame(height: 70)
- 
-                    Button("Add", action: {
-                        
-                        if let currentTikUser = firestoreManagerViewModel.currentTikUser {
-                            
-                            // save to firestore.
-                            let newTask = Task(title: title,
-                                               notes: notes,
-                                               location: location,
-                                               assignedTo: [currentTikUser],
-                                               setDate: setDate)
-                            
-                            firestoreManagerViewModel.saveTaskToFirestore(task: newTask)
-                            if addNotification {
-                                let unCenter = UserNotificationCenter()
-                                unCenter.setSingleAlert(task: newTask, time: notificationDate)
-                                print("Notification date: \(notificationDate)")
-                            }
-                            addTaskIsPresented = false
-                        }
-                    })
-                    .buttonStyle(CustomButtonStyle())
-                    .navigationBarItems(trailing: Button {
-                        addTaskIsPresented = false
-                    } label: {
-                        Image(systemName: "xmark")
-                            .foregroundColor(.black)
-                    })
-                    
                 }
+               
             }
-//            VStack {
-//                TextField("What to do:", text: $title)
-//                    .padding()
-//                TextField("How to do it:", text: $notes)
-//                    .padding()
-//                TextField("Where to do it:", text: $location)
-//                    .padding()
-//                HStack {
-//                    Spacer()
-//                    Text("When to do it:")
-//                    DatePicker("", selection: $setDate)
-//                    Spacer()
-//                }
-//                .padding()
-//                Spacer()
-//                Button("Add", action: {
-//
-//                    if let currentTikUser = firestoreManagerViewModel.currentTikUser {
-//
-//
-//                        // save to firestore.
-//                        let newTask = Task(title: title,
-//                                           notes: notes,
-//                                           location: location,
-//                                           assignedTo: [currentTikUser],
-//                                           setDate: setDate)
-//
-//                        firestoreManagerViewModel.saveTaskToFirestore(task: newTask)
-//                        addTaskIsPresented = false
-//                    }
-//
-//                })
-//                .navigationBarItems(trailing: Button {
-//                    addTaskIsPresented = false
-//                } label: {
-//                    Image(systemName: "xmark")
-//                })
-//            }
         }
-    }
-}
+    
 
 struct AddTaskView_Previews: PreviewProvider {
     static var previews: some View {
