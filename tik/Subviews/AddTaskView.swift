7bc8077f5af64eec4de3712378b75c114821bf3b
tobbesoren <113234609+tobbesoren@users.noreply.github.com>
2023-06-08 19:29:59 +0200

    viewRestored

--- tik/Subviews/AddTaskView.swift
+++ tik/Subviews/AddTaskView.swift
@@ -24,8 +24,9 @@ struct AddTaskView: View {
     
     var body: some View {
 //        NavigationView {
+        
             ZStack (alignment: .center) {
-                
+    
                 
                     Image("Two Phone Mockup Download App Instagram Post(10)")
                         .resizable()
@@ -41,7 +42,7 @@ struct AddTaskView: View {
                         .frame(width: 300, height: 300)
                         .offset(y: -180)
                     VStack(alignment: .center) {
-                        Spacer().frame(height: 90)
+                        Spacer().frame(height: 100)
                         TextField("What to do:", text: $title)
                             .font(.custom("Roboto-Bold", size: 22))
                             .multilineTextAlignment(.center)
@@ -75,7 +76,7 @@ struct AddTaskView: View {
                             }
                             
                         }
-                        Spacer().frame(height: 70)
+                        Spacer().frame(height: 10)
                         Button("Add", action: {
                             
                             if let currentTikUser = firestoreManagerViewModel.currentTikUser {
@@ -84,10 +85,15 @@ struct AddTaskView: View {
                                 let newTask = Task(title: title,
                                                    notes: notes,
                                                    location: location,
-                                                   assignedTo: [],
+                                                   assignedTo: [currentTikUser],
                                                    setDate: setDate)
                                 
                                 firestoreManagerViewModel.saveTaskToFirestore(task: newTask)
+                                if addNotification {
+                                    let unCenter = UserNotificationCenter()
+                                    unCenter.setSingleAlert(task: newTask, time: notificationDate)
+                                    print("Notification date: \(notificationDate)")
+                                }
                                 addTaskIsPresented = false
                             }
                         })
