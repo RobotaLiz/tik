976ed12b5d3f00372b201580eccc0469f8a14fa8
Tibalomium <ninjaharry@gmail.com>
2023-06-09 16:27:30 +0200

    Delete for custom view

--- tik/CalendarCustomView.swift
+++ tik/CalendarCustomView.swift
@@ -108,6 +108,13 @@ struct CalendarCustomView: View {
                 ForEach(calendarVM.tasks) { task in
                     TaskListRowView(task: task)
                 }
+                .onDelete() { indexSet in
+                    for index in indexSet {
+                        if let indexToRemove = calendarVM.allTasks.firstIndex(where: { $0.id == calendarVM.tasks[index].id}) {
+                            firestoreManagerVM.deleteTaskFromFirestore(index: indexToRemove)
+                        }
+                    }
+                }
                 Spacer().listRowBackground(Color.clear)
             }
             .scrollContentBackground(.hidden)
